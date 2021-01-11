c3d56252a6416646ef52b21343786e0d1e1a57d2
Jerid Fenderson <jrfenderson@gmail.com>
2021-01-11 11:42:27 -0500

    Added comments

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 
 namespace BlackJackCS
 {
+    // Created by Jerid Fenderson
+    // Suncoast Developers Guild
+    // Cohort-20
+    // Created January 8th, 2021
+    // Last modified January 11th, 2021
     class Program
     {
         class Player
